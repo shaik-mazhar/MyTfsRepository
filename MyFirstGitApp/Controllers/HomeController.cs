91c52156ad059dffe48d87322b491973ddbc4e0a
pacharla <venkat.pacharla@yahoo.co.in>
2015-10-30 14:12:20 +0530

    added two namespaces
    added two namespaces

--- MyFirstGitApp/Controllers/HomeController.cs
+++ MyFirstGitApp/Controllers/HomeController.cs
@@ -3,7 +3,8 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
-
+using System.Linq;
+using System.Web;
 namespace MyFirstGitApp.Controllers
 {
     public class HomeController : Controller
